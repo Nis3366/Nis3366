a61680e642570f1987b8412404411ddc23dd341d
Arcs-ur <arcs-ur@sjtu.edu.cn>
2025-04-01 16:54:06 +0800

    (feat)Data_management:mongodb integrated

--- Data_management/load_and_process.py
+++ Data_management/load_and_process.py
@@ -1,3 +1,17 @@
 import process_simple_text as pst
 import process_complex_text as pct
+import get_item_mongodb as get_item
 
+item_list, batch_size = get_item.get_items()
+emotion_count = {}
+location_count = {} 
+for i, item in enumerate(item_list):
+    emotion_max_index = pct.get_emotion(item['content_all'])['scores'].index(max(pct.get_emotion(item['content_all'])['scores']))
+    emotion_max = pct.get_emotion(item['content_all'])['labels'][emotion_max_index]
+    print(f"第 {i+1} 条文档：", item['content_all'],'\n',emotion_max,'\n')
+    #print(f"第 {i+1} 条文档：", item['content_all'],'\n',pct.get_emotion(item['content_all']),'\n')
+    emotion_count[emotion_max] = emotion_count.get(emotion_max, 0) + 1
+    location_count[item['location']] = location_count.get(item['location'], 0) + 1
+
+print(emotion_count)
+print(location_count)
