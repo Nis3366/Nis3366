997cd4872ec6b7602a892d25503eb51a2a031160
rui <wangxiaolu@sjtu.edu.cn>
2025-04-04 18:38:35 +0800

    <add> search page

---
+++ Front_end/pages/search.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from Crawler.get_top_lists import get_top_lists
+from Crawler.get_topic_posts import get_topic_posts
+st.title("Explore the Topics You Want to Learn About")
+
+# 添加搜索框
+search_query = st.text_input("请输入搜索内容", placeholder="输入关键词...")
+
+# 添加确认按钮
+if st.button("确认"):
+    if search_query: 
+        topic_content = search_query
+        get_topic_posts(topic_content)
+        st.success(f"已确认并存入数据库：{topic_content}")
+        if "selected_topic" in st.session_state:
+            print(st.session_state["selected_topic"])
+    else:
+        st.warning("请输入搜索内容！")
+
+ # details 按钮
+if st.button("详情"):
+    # 设置查询参数并跳转到 details.py
+    st.session_state["details_topics"] = search_query
+    st.page_link("pages/details.py", label="跳转到详情")
