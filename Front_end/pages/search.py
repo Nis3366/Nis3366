c4461a75c1b55454808d112f58ae0f974acaab4e
Hasturas <liyuefan@sjtu.edu.cn>
2025-04-06 15:22:48 +0800

    支持时间选择

--- Front_end/pages/search.py
+++ Front_end/pages/search.py
@@ -6,12 +6,17 @@ st.title("Explore the Topics You Want to Learn About")
 
 # 添加搜索框
 search_query = st.text_input("请输入搜索内容", placeholder="输入关键词...")
-
+# 添加日期范围输入框
+date_range = st.date_input("选择日期范围", [], help="选择开始日期和结束日期", key="date_range")
 # 添加确认按钮
 if st.button("确认"):
     if search_query: 
         topic_content = search_query
-        thread=threading.Thread(target=get_topic_posts, args=(topic_content,))
+        start_time, end_time = date_range if len(date_range) == 2 else (None, None)
+        if start_time and end_time:
+            thread=threading.Thread(target=get_topic_posts, args=(topic_content, start_time, end_time))
+        else:
+            thread=threading.Thread(target=get_topic_posts, args=(topic_content,))
         thread.start()
         st.success(f"已确认并存入数据库：{topic_content}")
         if "selected_topic" in st.session_state:
